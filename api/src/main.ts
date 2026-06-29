96cb1d2d9401436ef87f6106782b53665a881d3b
kajal12e <kajal.kumari4793@gmail.com>
2026-06-29 22:18:55 +0530

    fix:add AppModule import

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,7 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module'; 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
-  const port = process.env.PORT || 10000;
-  await app.listen(port, '0.0.0.0');
-  console.log(`App running on port ${port}`);
+  await app.listen(process.env.PORT || 10000, '0.0.0.0');
 }
 bootstrap();
