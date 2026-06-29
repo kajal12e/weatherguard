87ad11f6ccfd167d453ea9bd8f767fbdf362116f
kajal12e <kajal.kumari4793@gmail.com>
2026-06-29 22:46:05 +0530

    frontend ready for deploy

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module'; 
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const app = await NestFactory.create(AppModule);
+  
+  app.enableCors(); // Ye line sabse important hai
+
   await app.listen(process.env.PORT || 10000, '0.0.0.0');
 }
 bootstrap();
